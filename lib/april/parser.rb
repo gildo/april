c10fda0f715876fe99da55204cd5cdeb6082c2b6
Fyskij <fyskij@gmail.com>
2010-06-27 22:37:00 +0200

    uhm

--- lib/april/parser.rb
+++ lib/april/parser.rb
@@ -1,12 +1,30 @@
 module APRIL
-  module PARSER
+  def pong
+      write "PONG #{match[1]}"
+  end
 
-    def handle(s)
-      match = s.match(/PING\s+(.*)$/)
-      if match
-        write "PONG #{match[1]}"
-      end
+  def handle(match)
+    if match = /PING\s+(.*)$/
+      pong
+    elsif match = /^(.*)@#{nick}(.*)$/
+    	cicle_user(msg)
     end
   end
-end
 
+  def cicle_usr(msg)
+  	@users.each { |user|
+		 
+  end
+
+  def cicle_user(msg)
+  	t1 = Thread.new(cicle_usr(msg))
+  end
+  
+  def cicle_commands(msg)
+  
+  end
+  
+  def retrieve_users()
+  	write "NAMES @#{channel}"
+  end
+end
